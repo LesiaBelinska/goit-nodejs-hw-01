6f6a832c0b76b75217a4188e8b1db0905f5cd942
LesiaBelinska <lm.kudak@gmail.com>
2022-06-13 17:37:15 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+const contacts = require("./contacts");
+const { Command } = require('commander');
+
+const program = new Command();
+
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+
+    async function invokeAction({ action, id, name, email, phone }) {
+        switch (action) {
+            case 'list':
+                const result = await contacts.listContacts();
+                console.log(result);
+                break;
+
+            case 'get':
+                const contact = await contacts.getContactById(id);
+                console.log(contact);
+                break;
+
+            case 'add':
+                const newContact = await contacts.addContact(name, email, phone);
+                console.log(newContact);
+                break;
+
+            case 'remove':
+                const removeContact = await contacts.removeContact(id);
+                console.log(removeContact);
+                break;
+
+            default:
+                console.warn('\x1B[31m Unknown action type!');
+        }
+    }
+
+
+console.table(argv);
+invokeAction(argv);
+
+// # Получаем и выводим весь список контактов в виде таблицы (console.table)
+// node index.js --action list
+
+// # Получаем контакт по id
+// node index.js --action get --id 5
+
+// # Добавялем контакт
+// node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+// # Удаляем контакт
+// node index.js --action remove --id=3
+
