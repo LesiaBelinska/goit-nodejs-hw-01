73abee148d49b7dc1ef2018d2b9e22ec1cdfd2f7
LesiaBelinska <lm.kudak@gmail.com>
2022-06-13 17:37:12 +0300

    Create contacts.js

---
+++ contacts.js
@@ -0,0 +1,53 @@
+const fs = require("fs/promises");
+const path = require("path");
+const ObjectID = require("bson-objectid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+
+async function listContacts() {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+}
+
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const result = contacts.find(item => item.id === contactId);
+    if (!result) {
+        return null;
+    }
+    return result;
+}
+
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    const [removeContact] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return removeContact;
+
+}
+
+async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    const newContact = {
+        id: ObjectID(),
+        name,
+        email,
+        phone
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    addContact,
+    removeContact
+}
